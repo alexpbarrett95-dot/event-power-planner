8dc6ec1af257a8b5a4f85a877ecc7cf359e556d8
Alex Barrett <alexpbarrett95@gmail.com>
2026-06-08 21:50:46 +0100

    Initial Event Power Planner setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        padding: "40px",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1>Event Power Planner</h1>
+
+      <p>Web App Development Version</p>
+    </main>
+  );
+}
